6c6dd7536189c6413411f3c40f5de0b79d4fd355
Leeseonwoo11 <sw0679@naver.com>
2020-03-10 18:28:03 +0900

    Chapter2
    비행선이 우주공간을 비행하는 코드인데
    책을보고 공부하며 옮겼다.
    그런데 이미지를 불러오는 과정에서 오류가 생긴다.
    좀더 공부해야겠다....

---
+++ Chapter2/Chapter2/AnimSpriteComponent.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "SpriteComponent.h"
+#include <vector>
+
+class AnimSpriteComponent :public SpriteComponent
+{
+public:
+	AnimSpriteComponent(class Actor* owner, int drawOrder = 100);
+
+	void Update(float deltaTime) override;
+	void SetAnimTextures(const std::vector<SDL_Texture*>& textures);
+	float getAnimFPS() { return mAnimFPS; }
+	void SetAnimFPS(float fps) { mAnimFPS = fps; }
+
+private:
+	std::vector<SDL_Texture*>mAnimTextures;
+	float mCurrFrame;
+	float mAnimFPS;
+};
+
